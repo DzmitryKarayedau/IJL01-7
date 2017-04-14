437a67f7ef9a52003ef40ffb9422ec2e2bf9013d
siarhei.miashkou <siarhei.miashkou@leverx.com>
2017-04-14 10:38:59 +0300

    Change Main for working with PolindromUtils class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner in = new Scanner(System.in);
+        long inputNumber;
+
+        try {
+            System.out.print("Input number: ");
+            inputNumber = in.nextLong();
+            System.out.print("Number " + inputNumber + " is ");
+            if(PolindromUtils.checkOnPolindrom(inputNumber)){
+                System.out.println("polindrom");
+            }
+            else {
+                System.out.println("not polindrom");
+            }
+        } catch (InputMismatchException e) {
+            System.err.println("Wrong input data");
+        }
+
     }
 }
